68619b6cfd76b4aeaf4b492ff7e2171f35fd7718
Igor Gromadskiy <gromadskiyigor@gmail.com>
2025-01-17 18:44:58 +0200

    [Add] reasons section

--- src/components/Reasons/Reasons.tsx
+++ src/components/Reasons/Reasons.tsx
@@ -1,30 +1,44 @@
 import styles from './Reasons.module.css';
 import Image from 'next/image';
 import { reasonsItems } from '@/data/data';
+import Icon from '@/helpers/Icon';
 export default function Reasons() {
   return (
     <section id="reasons" className={styles.reasons}>
-      <div className={styles.container}>
-        <h2 className={styles.header}>4 dôvody, pre</h2>
-
-        <ul className={styles.list}>
-          {reasonsItems.map((item, index) => (
-            <li key={index}>
-              <h3 className={styles.parHead}>{item.head}</h3>
-              <div className={styles.reasonsImg}>
-                <Image
-                  src={item.img}
-                  width={0}
-                  height={0}
-                  sizes="100vw"
-                  alt="Deti v savane"
-                  priority
-                ></Image>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <ul className={styles.list}>
+        {reasonsItems.map((item, index) => (
+          <li className={styles.item} key={index}>
+            <Image
+              src={item.img}
+              width={0}
+              height={0}
+              sizes="100vw"
+              alt={item.head}
+              className={styles.image}
+              priority
+            ></Image>
+            <div className={styles.par}>
+              <h3 className={styles.par_head}>{item.head}</h3>
+              <p className={styles.par_text}>{item.text.first}</p>
+              <p className={styles.par_text_second}>{item.text.second}</p>
+            </div>
+            <div className={styles.button_wrap}>
+              <button className={styles.button} type="button">
+                <span className={styles.button_text}>{item.btn.first}</span>
+                <Icon name="icon-arrow-right" width={24} height={24} />
+              </button>
+              {item.btn.second && (
+                <button className={styles.button_support} type="button">
+                  <span className={styles.button_text_support}>
+                    {item.btn.second}
+                  </span>
+                  <Icon name="icon-arrow-right" width={24} height={24} />
+                </button>
+              )}
+            </div>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
